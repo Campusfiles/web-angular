daba09c7f77131f60ee147065625dde835544500
Ralph Largo <devlargs@gmail.com>
2019-04-18 01:48:51 +0800

    Created main  grid for home contents

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MatToolbarModule, MatIconModule } from '@angular/material';
-import { HeaderComponent } from './utilities/header/header.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FollowersComponent } from './components/followers/followers.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    FollowersComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
