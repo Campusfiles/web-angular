781d0fe1980a4421e67bc74c8e1dfc52e55a290f
Ralph Largo <devlargs@gmail.com>
2019-04-18 00:50:14 +0800

    Created header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MatToolbarModule } from '@angular/material';
+import { MatToolbarModule, MatIconModule } from '@angular/material';
+import { HeaderComponent } from './utilities/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    MatIconModule,
     MatToolbarModule
   ],
   providers: [],
