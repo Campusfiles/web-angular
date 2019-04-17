4d157bdb14b9ae30d8cdeb2f1da9a8a3c1c6081e
Ralph Largo <devlargs@gmail.com>
2019-04-18 02:47:43 +0800

    added followers and campus components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,21 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MatToolbarModule, MatIconModule } from '@angular/material';
+import { MatToolbarModule, MatIconModule, MatCardModule } from '@angular/material';
 import { HeaderComponent } from './components/header/header.component';
 import { FollowersComponent } from './components/followers/followers.component';
 import { HomeComponent } from './pages/home/home.component';
+import { CampusesComponent } from './components/campuses/campuses.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FollowersComponent,
-    HomeComponent
+    HomeComponent,
+    CampusesComponent
   ],
   imports: [
-    BrowserModule,
     AppRoutingModule,
+    BrowserModule,
+    MatCardModule,
     MatIconModule,
     MatToolbarModule
   ],
